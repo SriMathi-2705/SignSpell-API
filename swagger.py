800b726515bbe4d6891dda9c30ca3b91d95c256f
Sri Mathi <srimathi27052004@gmail.com>
2025-05-20 18:35:25 +0530

    Changes in swagger

--- swagger.py
+++ swagger.py
@@ -3,6 +3,5 @@ from flasgger import Swagger
 import os
 
 def init_swagger(app):
-    # Load swagger.yaml 
     yaml_path = os.path.join(os.path.dirname(__file__), 'swagger.yaml')
     Swagger(app, template_file=yaml_path)
